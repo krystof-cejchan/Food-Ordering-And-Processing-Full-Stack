d538ff0b77d5d347b23527ba563855aed5f38952
Kryštof Čejchan <krystofcejchan@seznam.cz>
2024-06-09 15:57:26 +0200

    scripts updated

--- customer_phone_ordering/phone_connection_to_localhost.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -u
-cd C:\Users\kryst\AppData\Local\Android\sdk\platform-tools
-adb reverse tcp:8080 tcp:8080
---
+++ phone_connection_to_localhost.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -u
+cd C:\Users\*\AppData\Local\Android\sdk\platform-tools
+adb reverse tcp:8080 tcp:8080
